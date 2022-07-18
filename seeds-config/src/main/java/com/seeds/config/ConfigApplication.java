93f5b81601a2f5ca06c4000ca7bd645956cbfcf8
y_k <819628513@qq.com>
2022-07-18 19:59:50 +0800

    配置config

---
+++ seeds-config/src/main/java/com/seeds/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.seeds.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author yk
+ * @email 819628513@qq.com
+ * @date 2022/07/13
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
