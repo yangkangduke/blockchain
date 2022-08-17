1baa9bac5e7a91c8a838888e7b654a1f562ceb02
y_k <819628513@qq.com>
2022-08-17 17:45:07 +0800

    增加注释

--- seeds-admin/src/main/java/com/seeds/admin/controller/ChainNftController.java
+++ seeds-admin/src/main/java/com/seeds/admin/controller/ChainNftController.java
@@ -1,11 +1,14 @@
 package com.seeds.admin.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.seeds.admin.dto.request.ChainBurnNftReq;
 import com.seeds.admin.dto.request.ChainMintNftReq;
 import com.seeds.admin.dto.request.ChainUpdateNftReq;
 import com.seeds.admin.service.ChainNftService;
 import com.seeds.common.dto.GenericDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +18,7 @@ import static com.seeds.admin.enums.AdminErrorCodeEnum.ERR_90001_FAIL_TO_EXECUTE
 
 
 @Slf4j
-@Api(tags = "区块链管理")
+@Api(tags = "区块链")
 @RestController
 @RequestMapping("/chain")
 public class ChainNftController {
@@ -24,13 +27,15 @@ public class ChainNftController {
     private ChainNftService chainNftService;
 
     @PostMapping("mint-nft")
+    @ApiOperation(value = "mint-nft", notes = "metaData参数格式：{\"name\":\"bow\", \"description\":\"shortbow\", \"attributes\": [{\"trait_type\":\"element\",\"value\":\"fire\"}]}")
     public GenericDto<Object> mintNewNft(
         @RequestPart("image") MultipartFile image,
-        @RequestPart("metaData") ChainMintNftReq request
+        @RequestParam String metaData
     ) {
-        log.info("metaData: {}", request);
+        log.info("metaData: {}", metaData);
+        ChainMintNftReq chainMintNftReq = JSONUtil.toBean(metaData, ChainMintNftReq.class);
         String imageFileHash = chainNftService.uploadImage(image);
-        String metadataFileHash = chainNftService.uploadMetadata(imageFileHash, request);
+        String metadataFileHash = chainNftService.uploadMetadata(imageFileHash, chainMintNftReq);
         boolean status = chainNftService.mintNft(metadataFileHash);
 
         if (status) {
@@ -41,6 +46,7 @@ public class ChainNftController {
     }
 
     @PostMapping("/update-nft")
+    @ApiOperation(value = "update-nft", notes = "update-nft")
     public GenericDto<Object> updateNftAttribute(@RequestBody ChainUpdateNftReq request) {
         String imageFileHash = chainNftService.getMetadataFileImageHash(request.getTokenId());
         String metadataFileHash = chainNftService.updateMetadata(imageFileHash, request);
@@ -54,6 +60,7 @@ public class ChainNftController {
     }
 
     @PostMapping("/burn-nft")
+    @ApiOperation(value = "burn-nft", notes = "burn-nft")
     public GenericDto<Object> burnNft(@RequestBody ChainBurnNftReq request) {
         boolean status = chainNftService.burnNft(request.getTokenId());
 
@@ -65,6 +72,7 @@ public class ChainNftController {
     }
 
     @GetMapping("/test-auth")
+    @ApiOperation(value = "test-auth", notes = "test-auth")
     public GenericDto<Object> testAuth() {
         try {
             return GenericDto.success(chainNftService.testAuth());
