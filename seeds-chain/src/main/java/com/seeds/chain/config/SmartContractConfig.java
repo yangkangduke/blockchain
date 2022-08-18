1e43eee11a68d756bc58854466efe78744c6da72
y_k <819628513@qq.com>
2022-08-18 11:11:33 +0800

    chain的配置修改

--- seeds-chain/src/main/java/com/seeds/chain/config/SmartContractConfig.java
+++ seeds-chain/src/main/java/com/seeds/chain/config/SmartContractConfig.java
@@ -1,28 +1,22 @@
 package com.seeds.chain.config;
 
 import lombok.Data;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Slf4j
 @Data
-@ToString
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "smart-contract")
 public class SmartContractConfig {
-    @Value("${smart-contract.chain-id:97}")
     long chainId;
-    @Value("${smart-contract.owner.address:0x00}")
     String ownerAddress;
-    @Value("${smart-contract.owner.private-key:0x00}")
     String ownerPrivateKey;
-    @Value("${smart-contract.rpc-provider.url:http://localhost}")
     String rpcProviderUrl;
-    @Value("${smart-contract.game-address:0x00}")
-    String gameItemsContractAddress;
+    String gameAddress;
 
     @PostConstruct
     public void init() {
