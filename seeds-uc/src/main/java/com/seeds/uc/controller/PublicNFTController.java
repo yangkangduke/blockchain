cf016fc0249b332d5872c22e64a38afee855fc06
hang.yu <1462771551@qq.com>
2022-08-19 16:05:13 +0800

    update

--- seeds-uc/src/main/java/com/seeds/uc/controller/PublicNFTController.java
+++ seeds-uc/src/main/java/com/seeds/uc/controller/PublicNFTController.java
@@ -1,7 +1,15 @@
 package com.seeds.uc.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.seeds.admin.dto.request.UcNftPageReq;
+import com.seeds.admin.dto.response.SysNftResp;
+import com.seeds.admin.feign.RemoteNftService;
+import com.seeds.common.dto.GenericDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +27,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicNFTController {
 
 
+    @Autowired
+    private RemoteNftService remoteNftService;
 
+    @ApiOperation(value = "分页查询", notes = "分页查询")
+    @GetMapping("/uc-page")
+    public GenericDto<IPage<SysNftResp>> getSysFilePage(UcNftPageReq query) {
+        return remoteNftService.ucPage(query);
+    }
 
 
 }
