5bd204c2b08a24c6b0429c79588c2ad02fb676bf
y_k <819628513@qq.com>
2022-09-21 12:35:23 +0800

    新增account服务

--- seeds-common/src/main/java/com/seeds/common/interceptor/ClientSignRequestInterceptor.java
+++ seeds-common/src/main/java/com/seeds/common/interceptor/ClientSignRequestInterceptor.java
@@ -1,44 +1,44 @@
-//package com.seeds.common.interceptor;
-//
-//import com.seeds.common.crypto.RequestConfiguration;
-//import com.seeds.common.crypto.SignatureHandler;
-//import com.seeds.common.crypto.TimeUtils;
-//import feign.RequestInterceptor;
-//import feign.RequestTemplate;
-//import lombok.extern.slf4j.Slf4j;
-//
-///**
-// * @author rickierao
-// * @email antilaw@yahoo.com
-// * @date 2021/1/25
-// */
-//@Slf4j
-//public class ClientSignRequestInterceptor implements RequestInterceptor {
-//    private final String accessKey;
-//    private final String secretKey;
-//    private final RequestConfiguration requestConfiguration;
-//
-//    public ClientSignRequestInterceptor(String accessKey, String secretKey, RequestConfiguration requestConfiguration) {
-//        this.accessKey = accessKey;
-//        this.secretKey = secretKey;
-//        this.requestConfiguration = requestConfiguration;
-//    }
-//
-//    @Override
-//    public void apply(RequestTemplate requestTemplate) {
-//        log.debug("apply request interceptor apply");
-//        String path = requestTemplate.path();
-//        String timestamp = TimeUtils.getCurrentTimestamp();
-//        String method = requestTemplate.method();
-//        byte[] content = requestTemplate.body();
-//        String signature = requestConfiguration.getVersion().isDataInSignature()
-//                ? SignatureHandler.generate(secretKey, method, timestamp, path, content)
-//                : SignatureHandler.generate(secretKey, method, timestamp, path);
-//        log.debug("request sig is: {}", signature);
-//
-//        requestTemplate.header(requestConfiguration.getAccessKeyHttpHeader(), accessKey);
-//        requestTemplate.header(requestConfiguration.getTimestampHttpHeader(), timestamp);
-//        requestTemplate.header(requestConfiguration.getSignatureHttpHeader(), signature);
-//        requestTemplate.header(requestConfiguration.getVersionHttpHeader(), requestConfiguration.getVersion().getValue());
-//    }
-//}
+package com.seeds.common.interceptor;
+
+import com.seeds.common.crypto.RequestConfiguration;
+import com.seeds.common.crypto.SignatureHandler;
+import com.seeds.common.crypto.TimeUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author rickierao
+ * @email antilaw@yahoo.com
+ * @date 2021/1/25
+ */
+@Slf4j
+public class ClientSignRequestInterceptor implements RequestInterceptor {
+    private final String accessKey;
+    private final String secretKey;
+    private final RequestConfiguration requestConfiguration;
+
+    public ClientSignRequestInterceptor(String accessKey, String secretKey, RequestConfiguration requestConfiguration) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.requestConfiguration = requestConfiguration;
+    }
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        log.debug("apply request interceptor apply");
+        String path = requestTemplate.path();
+        String timestamp = TimeUtils.getCurrentTimestamp();
+        String method = requestTemplate.method();
+        byte[] content = requestTemplate.body();
+        String signature = requestConfiguration.getVersion().isDataInSignature()
+                ? SignatureHandler.generate(secretKey, method, timestamp, path, content)
+                : SignatureHandler.generate(secretKey, method, timestamp, path);
+        log.debug("request sig is: {}", signature);
+
+        requestTemplate.header(requestConfiguration.getAccessKeyHttpHeader(), accessKey);
+        requestTemplate.header(requestConfiguration.getTimestampHttpHeader(), timestamp);
+        requestTemplate.header(requestConfiguration.getSignatureHttpHeader(), signature);
+        requestTemplate.header(requestConfiguration.getVersionHttpHeader(), requestConfiguration.getVersion().getValue());
+    }
+}
--- seeds-wallet/src/main/java/com/seeds/wallet/config/WebMvcConfig.java
+++ seeds-wallet/src/main/java/com/seeds/wallet/config/WebMvcConfig.java
@@ -1,30 +1,30 @@
-//package com.seeds.wallet.config;
-//
-//import com.seeds.common.interceptor.ServerApiAuthInterceptor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//
-///**
-// * @author rickierao
-// * @email antilaw@yahoo.com
-// * @date 2021/1/25
-// */
-//@Configuration
-//public class WebMvcConfig implements WebMvcConfigurer {
-//    @Autowired
-//    private ServerApiAuthInterceptor apiAuthInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        List<String> excludePatterns = new LinkedList<>();
-//        excludePatterns.add("/info");
-//        excludePatterns.add("/health");
-//        excludePatterns.add("/error");
-//        registry.addInterceptor(apiAuthInterceptor).excludePathPatterns(excludePatterns);
-//    }
-//}
+package com.seeds.wallet.config;
+
+import com.seeds.common.interceptor.ServerApiAuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author rickierao
+ * @email antilaw@yahoo.com
+ * @date 2021/1/25
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private ServerApiAuthInterceptor apiAuthInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> excludePatterns = new LinkedList<>();
+        excludePatterns.add("/info");
+        excludePatterns.add("/health");
+        excludePatterns.add("/error");
+        registry.addInterceptor(apiAuthInterceptor).excludePathPatterns(excludePatterns);
+    }
+}
