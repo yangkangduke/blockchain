9a9b516a14aa60d98463c8dd68c5c21bd9be749d
y_k <819628513@qq.com>
2022-11-02 16:04:46 +0800

    黑地址bug fix

--- seeds-account/src/main/java/com/seeds/account/service/IBlacklistAddressService.java
+++ seeds-account/src/main/java/com/seeds/account/service/IBlacklistAddressService.java
@@ -22,7 +22,7 @@ public interface IBlacklistAddressService extends IService<BlacklistAddress> {
      */
     List<BlacklistAddressDto> loadAll();
 
-    BlacklistAddressDto get(int type, long userId, String address);
+    BlacklistAddressDto get(Integer type, Long userId, String address);
 
     /**
      * 从缓存获取
