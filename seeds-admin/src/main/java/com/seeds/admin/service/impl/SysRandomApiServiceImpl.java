8b3f878c18465df82a3f2bc2afc4948498ad6264
hang.yu <1462771551@qq.com>
2022-11-07 17:56:09 +0800

    update

---
+++ seeds-admin/src/main/java/com/seeds/admin/service/impl/SysRandomApiServiceImpl.java
@@ -0,0 +1,30 @@
+package com.seeds.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.seeds.admin.entity.SysGameApiEntity;
+import com.seeds.admin.mapper.SysGameApiMapper;
+import com.seeds.admin.service.SysGameApiService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 游戏api
+ *
+ * @author hang.yu
+ * @date 2022/10/10
+ */
+@Service
+public class SysGameApiServiceImpl extends ServiceImpl<SysGameApiMapper, SysGameApiEntity> implements SysGameApiService {
+
+    @Override
+    public String queryUrlByGameAndType(Long gameId, Integer type) {
+        LambdaQueryWrapper<SysGameApiEntity> queryWrap = new QueryWrapper<SysGameApiEntity>().lambda()
+                .eq(SysGameApiEntity::getGameId, gameId)
+                .eq(SysGameApiEntity::getType, type);
+        SysGameApiEntity gameApi = getOne(queryWrap);
+        return gameApi.getBaseUrl() + gameApi.getApi();
+    }
+}
+
