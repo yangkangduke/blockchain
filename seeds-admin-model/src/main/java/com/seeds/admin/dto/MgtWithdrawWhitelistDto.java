57712d3360b1922c976dbcfdf95a4573f5a2b2e1
yang.deng <769151627@qq.com>
2022-11-09 17:09:01 +0800

    添加swagger注释

--- seeds-admin-model/src/main/java/com/seeds/admin/dto/MgtWithdrawWhitelistDto.java
+++ seeds-admin-model/src/main/java/com/seeds/admin/dto/MgtWithdrawWhitelistDto.java
@@ -3,6 +3,7 @@ import com.google.gson.Gson;
 import com.seeds.account.enums.CommonStatus;
 import com.seeds.admin.annotation.MgtNumberValidator;
 import com.seeds.admin.audit.Auditable;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,21 +19,28 @@ import java.math.BigDecimal;
 public class MgtWithdrawWhitelistDto implements Auditable {
 
     @NotNull
+    @ApiModelProperty("用户id")
     private Long userId;
     @NotNull
+    @ApiModelProperty("币种")
     private String currency;
     @NotNull
     @MgtNumberValidator
+    @ApiModelProperty("最大额度")
     private BigDecimal maxAmount;
     @NotNull
 
     @MgtNumberValidator
+    @ApiModelProperty("单日额度")
     private BigDecimal intradayAmount;
     @NotNull
     @MgtNumberValidator
+    @ApiModelProperty("自动额度，不用走人工审批的额度")
     private BigDecimal autoAmount;
+    @ApiModelProperty("评语")
     private String comments;
     //1,启用，2，停用
+    @ApiModelProperty("状态，1:启用 2：停用")
     private CommonStatus status;
 
 
