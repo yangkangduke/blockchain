bc2271cfe0f993ea9c258dde1d7e0935ba2ebe9c
yang.deng <769151627@qq.com>
2022-11-15 12:01:45 +0800

    update

--- seeds-account-model/src/main/java/com/seeds/account/dto/BlacklistAddressDto.java
+++ seeds-account-model/src/main/java/com/seeds/account/dto/BlacklistAddressDto.java
@@ -1,7 +1,7 @@
 package com.seeds.account.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.seeds.account.enums.CommonStatus;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,16 +23,24 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class BlacklistAddressDto implements Serializable {
     private static final long serialVersionUID = -1L;
+    private Long id;
 
+    @ApiModelProperty(value = "1；冲币 2：提币")
     private Integer type;
 
     @NotNull
     private Long userId;
 
     @NotNull
+    @ApiModelProperty("address")
     private String address;
 
     @NotNull
+    @ApiModelProperty("reason")
     private String reason;
+
+    @ApiModelProperty(value = "1:启用 2：停用")
     private Integer status;
+    @ApiModelProperty(value = "1：ETH 3：TRON")
+    private Integer chain;
 }
