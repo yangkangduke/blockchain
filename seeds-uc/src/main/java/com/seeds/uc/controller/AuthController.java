09eaf2beee8437b332a0fe016a5342e91ad37036
y_k <819628513@qq.com>
2022-12-25 08:10:52 +0800

    登陆加上消息

--- seeds-uc/src/main/java/com/seeds/uc/controller/AuthController.java
+++ seeds-uc/src/main/java/com/seeds/uc/controller/AuthController.java
@@ -26,6 +26,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -33,6 +37,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -50,6 +56,8 @@ public class AuthController {
     private CacheService cacheService;
     @Autowired
     private IUsUserLoginLogService userLoginLogService;
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
 
 
     @PostMapping("/register/email")
@@ -92,6 +100,33 @@ public class AuthController {
         LoginResp login = ucUserService.login(loginReq);
         if (login.getUcToken() == null) {
             return GenericDto.failure(UcErrorCodeEnum.ERR_10070_PLEASE_ENTER_2FA.getDescEn(), UcErrorCodeEnum.ERR_10070_PLEASE_ENTER_2FA.getCode(), login);
+        }
+        // 生产登陆成功消息
+        try {
+            Map loginMap = new HashMap<>();
+            loginMap.put("userIp",loginReq.getUserIp());
+            loginMap.put("serviceRegion",loginReq.getServiceRegion());
+            ListenableFuture<SendResult> listenableFuture = kafkaTemplate.send("login_topic", loginMap);
+            // 提供回调方法，可以监控消息的成功或失败的后续处理
+            listenableFuture.addCallback(new ListenableFutureCallback<SendResult>() {
+                @Override
+                public void onFailure(Throwable throwable) {
+                    log.info("发送消息失败，" + throwable.getMessage());
+                }
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    // 消息发送到的topic
+                    String topic = sendResult.getRecordMetadata().topic();
+                    // 消息发送到的分区
+                    int partition = sendResult.getRecordMetadata().partition();
+                    // 消息在分区内的offset
+                    long offset = sendResult.getRecordMetadata().offset();
+                    log.info(String.format("发送消息成功，topc：%s, partition: %s, offset：%s ", topic, partition, offset));
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return GenericDto.success(login);
     }
