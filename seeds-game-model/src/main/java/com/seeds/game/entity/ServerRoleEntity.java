8d88348ff3bf266a57d89ffd8986bf1b53736af1
hewei <952517823@qq.com>
2023-03-01 17:21:10 +0800

    fix

--- seeds-game-model/src/main/java/com/seeds/game/entity/ServerRoleEntity.java
+++ seeds-game-model/src/main/java/com/seeds/game/entity/ServerRoleEntity.java
@@ -33,8 +33,14 @@ public class ServerRoleEntity implements Serializable {
     @ApiModelProperty("大区")
     private Integer region;
 
+    @ApiModelProperty("大区名字")
+    private String regionName;
+
     @ApiModelProperty("游戏服")
     private Integer gameServer;
+
+    @ApiModelProperty("游戏服名字")
+    private String gameServerName;
 
     @ApiModelProperty("等级")
     private Integer level;
