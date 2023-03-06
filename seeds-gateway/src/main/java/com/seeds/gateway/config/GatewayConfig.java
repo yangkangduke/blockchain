cb1802c60351821cabfee151f1507de1b0109e1f
hewei <952517823@qq.com>
2023-03-06 21:17:30 +0800

    修改限流配置

--- seeds-gateway/src/main/java/com/seeds/gateway/config/GatewayConfig.java
+++ seeds-gateway/src/main/java/com/seeds/gateway/config/GatewayConfig.java
@@ -15,6 +15,10 @@ public class GatewayConfig {
     @Bean(name = "defaultResolver")
     public KeyResolver defaultResolver() {
         return exchange -> {
+            final String path = exchange.getRequest().getURI().getPath();
+            if (path.contains("/public/file/download")){
+                return Mono.just(exchange.getRequest().getURI().getQuery());
+            }
             String userId = exchange.getRequest().getHeaders().getFirst(HttpHeaders.INTERNAL_USER_ID);
             if (StringUtils.hasText(userId)) {
                 return Mono.just(userId);
@@ -23,11 +27,7 @@ public class GatewayConfig {
             if (StringUtils.hasText(userId)) {
                 return Mono.just(userId);
             }
-            final String path = exchange.getRequest().getURI().getPath();
-            if ( path.contains("/admin/public/file/download")){
-                log.info("path--->" + path);
-                return Mono.just(path);
-            }
+
             return Mono.just(GatewayUtils.getClientIp(exchange));
         };
     }
