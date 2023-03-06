1de1f5382ad55cddf2b6b5455b63faa2fa83d3c8
hewei <952517823@qq.com>
2023-03-06 20:35:49 +0800

    修改限流配置

--- seeds-gateway/src/main/java/com/seeds/gateway/config/GatewayConfig.java
+++ seeds-gateway/src/main/java/com/seeds/gateway/config/GatewayConfig.java
@@ -2,6 +2,7 @@ package com.seeds.gateway.config;
 
 import com.seeds.common.web.HttpHeaders;
 import com.seeds.gateway.util.GatewayUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +10,21 @@ import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class GatewayConfig {
     @Bean(name = "defaultResolver")
     public KeyResolver defaultResolver() {
         return exchange -> {
             String userId = exchange.getRequest().getHeaders().getFirst(HttpHeaders.INTERNAL_USER_ID);
             if (StringUtils.hasText(userId)) {
-                final String path = exchange.getRequest().getURI().getPath();
-                return Mono.just(path);
+
+                return Mono.just(userId);
             }
             userId = exchange.getRequest().getHeaders().getFirst(HttpHeaders.ADMIN_USER_ID);
             if (StringUtils.hasText(userId)) {
-                return Mono.just(userId);
+                final String path = exchange.getRequest().getURI().getPath();
+                log.info("path--->" + path);
+                return Mono.just(path);
             }
             return Mono.just(GatewayUtils.getClientIp(exchange));
         };
