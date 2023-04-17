36a0ca7f3459325cb90d0813aa556ac3fe72367c
hang.yu <1462771551@qq.com>
2023-04-17 12:10:58 +0800

    update

---
+++ seeds-game/src/main/java/com/seeds/game/config/SolanaConfig.java
@@ -0,0 +1,27 @@
+package com.seeds.game.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Data
+@Configuration
+public class SolanaConfig {
+
+    @Value("${solana.equipment.contract.address:DUzYwZxVpBmyKjhBWJEDHWV88v88C72iNsZskEMx6ySb}")
+    private String equipmentContractAddress;
+
+    @Value("${solana.skin.contract.address:A5pEUGqsUUmeioTAQxCiNJKotwDQD7GZPehXUykUxoiA}")
+    private String skinContractAddress;
+
+    @Value("${solana.token.standard:Metaplex}")
+    private String tokenStandard;
+
+    @Value("${solana.chain:Solana}")
+    private String chain;
+
+    @Value("${solana.creator.earnings:5%}")
+    private String creatorEarnings;
+
+}
