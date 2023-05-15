157f2699ce954940ccafe6e12cf34c3cd8d0f870
hang.yu <1462771551@qq.com>
2023-05-15 16:46:23 +0800

    update

--- seeds-game/src/main/java/com/seeds/game/config/SeedsApiConfig.java
+++ seeds-game/src/main/java/com/seeds/game/config/SeedsApiConfig.java
@@ -21,7 +21,7 @@ public class SeedsApiConfig {
     @Value("${seeds.api.solTokenAddress:So11111111111111111111111111111111111111112}")
     private String solTokenAddress;
 
-    @Value("${seeds.api.baseDomain:https://seed-api-test.llyc.fun}")
+    @Value("${seeds.api.baseDomain:http://192.168.6.100:8002}")
     private String baseDomain;
 
     @Value("${seeds.api.placeOrder:/api/chainOp/placeOrder}")
