5b673e9c8c0507835b2c2cdcc5eb693c3e077f0b
hang.yu <1462771551@qq.com>
2023-06-16 10:24:04 +0800

    配置

--- seeds-gateway/src/main/java/com/seeds/gateway/filter/RequestTimingFilter.java
+++ seeds-gateway/src/main/java/com/seeds/gateway/filter/RequestTimingFilter.java
@@ -1,9 +1,12 @@
 package com.seeds.gateway.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.i18n.SimpleLocaleContext;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
+import java.util.Locale;
 import java.util.UUID;
 
 @Slf4j
@@ -19,6 +23,7 @@ public class RequestTimingFilter implements GlobalFilter, Ordered {
 
     public static final String REQ_UUID = "REQ-UUID";
     public static final String REQ_RECV_TIMESTAMP = "REQ-RECV-TIMESTAMP";
+    public static final String CONTENT_LANGUAGE = "content-language";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -33,6 +38,15 @@ public class RequestTimingFilter implements GlobalFilter, Ordered {
                     httpHeaders.set(REQ_RECV_TIMESTAMP, nowTimestampValue);
                 })
                 .build();
+
+        // 语言国际化
+        String language = exchange.getRequest().getHeaders().getFirst(CONTENT_LANGUAGE);
+        Locale locale = Locale.getDefault();
+        if (StringUtils.isNotBlank(language)) {
+            String[] split = language.split("_");
+            locale = new Locale(split[0], split[1]);
+        }
+        LocaleContextHolder.setLocaleContext(new SimpleLocaleContext(locale), true);
 
         return chain.filter(exchange.mutate().request(request).build())
                 .then(Mono.fromRunnable(() ->
