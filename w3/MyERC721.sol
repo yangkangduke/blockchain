6b6887dd773a02c980a54c27b896836cc65501f6
yk <yangkang.duke@qq.com>
2023-08-22 13:26:52 +0800

    增加注释

---
+++ w3/MyERC721.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+
+contract MyERC721 is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721(unicode"yangkang", "yk") {}
+
+    //  QmZNFPwox146ohY93ViFD8omSThRAVYF1A96MNHbWoa2Nr
+
+    // ipfs://QmT4YDZ2dgTSpfHwPndnSuvHrAXNvtDBKNDUwN8nuZiVHT
+    function mint(address student, string memory tokenURI) public returns (uint256){
+        uint256 newItemId = _tokenIds.current();
+        _mint(student, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
